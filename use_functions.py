51c74d83af89d96c0528510c787dd70679e190b5
antonSpirin <spirin.soft@gmail.com>
2023-02-08 09:14:34 +0300

    Save

--- use_functions.py
+++ use_functions.py
@@ -34,20 +34,57 @@
 Для реализации основного меню можно использовать пример ниже или написать свой
 """
 
+
+history_transaction = []
+
+def transaction(history_transaction, positiv_negativ_numb):
+    balance = 0
+    for i in range(len(history_transaction)):
+        balance += history_transaction[i]
+    if positiv_negativ_numb > 0:
+        add_transaction = float(input('Ввведите сумму , которую вы хотите внести на ваш счёт: '))
+        history_transaction.append(add_transaction)
+    else:
+        add_transaction = float(input('Ввведите сумму вашей покупки: '))
+        if add_transaction < balance:
+            add_transaction*-1
+            history_transaction.append(add_transaction)
+        else:
+            print('Сумма покупки превышает ваш баланс')
+    #print(history_transaction)
+def balance_func (history_transaction):
+    balance=0
+    for i in range(len(history_transaction)):
+        balance += history_transaction[i]
+    print(f'Ваш баланс: {balance} $')
+    return balance
+
+
+
+balance = 0
+print(f'Добро пожаловать в ваш электроный кошелек!\n На вашем счету: {balance} $')
+
 while True:
     print('1. пополнение счета')
     print('2. покупка')
     print('3. история покупок')
     print('4. выход')
 
-    choice = input('Выберите пункт меню')
+    choice = input('Выберите пункт меню: ')
     if choice == '1':
+        transaction(history_transaction, 1)
+        balance_func(history_transaction)
         pass
     elif choice == '2':
+        transaction(history_transaction, -1)
+        balance_func(history_transaction)
         pass
+
     elif choice == '3':
         pass
     elif choice == '4':
         break
     else:
         print('Неверный пункт меню')
+
+print(history_transaction)
